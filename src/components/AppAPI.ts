fdb20e21a5078ec3738f540d22e1b0ca7a4ba566
byb4ek <157111926+byb4ek@users.noreply.github.com>
2024-07-20 23:25:26 +0300

    Помогите понять все это добро)

---
+++ src/components/AppAPI.ts
@@ -0,0 +1,24 @@
+import { IOrderResult } from './../types/index';
+import { IOrder, IOrderResult, IProductItem } from "../types";
+import { Api, ApiListResponse } from "./base/api";
+
+//Класс API
+export class AppAPI extends Api {
+	readonly api: Api;
+	constructor(api: Api, baseUrl: string, options: RequestInit = {}) {
+		super(baseUrl, options);
+		this.api = api;
+	}
+
+	getProductList() {
+		return this.get('/product').then((response: ApiListResponse<IProductItem>) => {
+			return response.items.map((item) => {
+				return { ...item };
+			})
+		})
+	};
+
+	orderProducts(order: IOrder): Promise<IOrderResult> {
+		return this.post('/order', order).then((response: IOrderResult) => response)
+	};
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,78 +1,43 @@
-//Отразить типы в этой папке
-//описание этих типов вместе с документацией и будет первым шагом
-// Описание
 
-/* 1.Типов данных с которыми будете работать в приложении. Как минимум у вас
- должны быть описаны объекты приходящие к вам через API и объекты выводимые на экране. 
-	 2.Ваши модели в итоге должны будут трансформировать один тип в другой.
-	 3.Интерфейс API-клиента
-	 4.Интерфейсы модели данных
-	 5.Интерфейсы отображений
-	 6.Интерфейсы базовых классов
-	 7.Перечисление событий и их интерфейсы (если используете брокер)
-	 8.Любые другие типы и интерфейсы если вы заложили их в архитектуру */
-
-
-//Описывает данные карточки 
-export interface ICard {
-	id: string;
-	description: string;
-	image: string;
-	title: string;
-	category: string;
-	price: number | null;
+export interface IProductItem {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+  price: number | null;
 }
 
-//Описывает данные покупки 
-export interface IBuyDetails {
-	payment: string;
-	email: string;
-	phone: string;
-	address: string;
-	total: number;
-	items: string[];
+export interface IAppDate {
+  catalog: IProductItem[];
+  preview: string;
+  basket: string[];
+  order: IOrder;
+  total: string | number;
+  loading: boolean;
 }
 
-// интерфейс для вывода массива карточек на страницу
-export interface ICardsData {
-	cards: ICard[];
-	preview: string | null;
-	getCard(cardId: string): ICard;
+
+export interface IProductsList {
+  products: IProductItem[];
 }
 
-//интерфейс для работы с формой заказа
-export interface IOrderInfo{
-	//submitButton: HTMLButtonElement;
-	getOrderInfo():TModalOrderInfo;
-	setOrderInfo(orderData:TModalOrderInfo):void;
-	checkUserValidation(data: Record<keyof TModalOrderInfo, string>): boolean;
+
+export interface IOrderForm {
+  payment?: string;
+  address?: string;
+  phone?: string;
+  email?: string;
+  total?: string | number;
 }
 
-//интерфейс для работы с формой данных заказчика
-export interface ICastomerInfo{
-	//submitButton: HTMLButtonElement;
-	getCastomerInfo():TModalCastomerInfo;
-	setCastomerInfo(orderData:TModalCastomerInfo):void;
-	checkUserValidation(data: Record<keyof TModalCastomerInfo, string>): boolean;
+export interface IOrder extends IOrderForm {
+  items: string[];
 }
 
-//Тип данных используемый на главной странице
-export type TPublicInfo = Pick<ICard, 'id' | 'image' | 'title' | 'category' | 'price'>;
 
-//Тип данных используемый в модальном окне Способ оплаты, Адрес
-export type TModalOrderInfo = Pick<IBuyDetails, 'payment' | 'address'>;
+export type FormErrors = Partial<Record<keyof IOrder, string>>;
 
-//Тип данных используемый в модальном окне Почта, Телефон
-export type TModalCastomerInfo = Pick<IBuyDetails, 'email' | 'phone'>;
-
-//Тип данных используемый в модальном окне Успешная оплата
-export type TModalBuySuccess = Pick<IBuyDetails, 'total'>;
-
-//Тип данных используемый в модальном окне Корзины
-export type TModalBasket = Pick<ICard, 'id' | 'title' | 'price'>;
-
-//Как сюда запихнуть общую стоимость????
-
-
-//Тип данных используемый в модальном окне самой Карточки
-//export type TModalCard = ICard;
+export interface IOrderResult {
+  id: string;
+}
